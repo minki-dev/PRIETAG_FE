88c5bbb236d2c64a7e12a3374fd811c6c02e5fa0
김민기 <kimminki.dev@gmail.com>
2023-06-05 23:36:35 +0900

    🔨 fix: prettier, eslint 충돌 오류 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,18 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-	title: 'EZFEE',
-	description: 'No code saas service price tag making service',
+  title: 'EZFEE',
+  description: 'No code saas service price tag making service',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="ko">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }

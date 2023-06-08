0ba97b7f53efda8913bc44afb482f0357ce3fc0c
sung34 <imspins1234@gmail.com>
2023-06-08 23:42:01 +0900

    :gear: conf: font 적용 확인 및 next auth path init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header';
+
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -16,7 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <Header />
       <body className={inter.className}>{children}</body>
     </html>
   );

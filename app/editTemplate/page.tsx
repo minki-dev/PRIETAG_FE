fa55985e3bc8e675d4aed7f69eeccda5d47ce821
sung34 <imspins1234@gmail.com>
2023-06-16 01:36:27 +0900

    :truck: rename: 반응형 레이아웃에 맞는 depth로 변경

--- app/editTemplate/page.tsx
+++ app/editTemplate/page.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import FAQ from './components/FAQ';
-import RightMenu from './components/RightMenu'
+import RightMenu from './components/RightMenu';
 
-export default function TemplateEdit() {
+import DraggableArea from './components/DraggableArea';
+import Table from './components/Table/Table';
+
+export default function EditTemlate() {
+	
 	return (
-    <div>
-      <div>TemplateEdit</div>
-      <RightMenu />
-			<FAQ />
-		</div>
+		<main className="flex w-[calc(100vw-14.5rem)] flex-col justify-center">
+			
+				{/* <RightMenu /> */}
+				<section className='editable-outer '>
+					
+					{/* priceCardArea */}
+					<DraggableArea areaType="priceCardArea" />
+				</section>
+				<section className='editable-outer' >
+					<DraggableArea areaType="tableArea" />
+					<Table />
+				</section>
+				<section className='editable-outer'>
+					<DraggableArea areaType="faqArea" />
+					<FAQ />
+				</section>
+
+		</main>
 	);
 }

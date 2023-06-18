e6f2d1e94c7697a45b25d954990fe2dbc84a6554
김민기 <kimminki.dev@gmail.com>
2023-06-19 08:00:32 +0900

    :factory: refactor: 테스트 부분 주석처리 및 린팅
    - editTemplate 페이지에 테스트 코드 주석처리 및 priceModal 버튼 추가(기능 테스트 위함)
    - priceModal.tsx 린팅

--- app/editTemplate/page.tsx
+++ app/editTemplate/page.tsx
@@ -38,32 +38,32 @@ export default function EditTemplate() {
 		setIsModalOpen(!isModalOpen);
 	};
 	// 아래의 데이터 목록은 api로부터 받아오는 데이터임을 명심할 것
-	const yearDiscount = 10;
-	const cardCount = 2;
-	const headDiscount = [
-		{ headCount: 5, discountRate: 10 },
-		{ headCount: 10, discountRate: 20 },
-		{ headCount: 15, discountRate: 30 },
-		{ headCount: 20, discountRate: 40 },
-	];
-	const testCard: PriceCard[] = [
-		{
-			title: '테스트 카드',
-			price: 10000,
-			discountRate: 10,
-			detail: '테스트 카드 상세',
-			feature: '테스트 카드 특징',
-			content: ['테스트 카드 내용1', '테스트 카드 내용2', '테스트 카드 내용3'],
-		},
-		{
-			title: '테스트 카드',
-			price: 10000,
-			discountRate: 10,
-			detail: '테스트 카드 상세',
-			feature: '테스트 카드 특징',
-			content: ['테스트 카드 내용1', '테스트 카드 내용2', '테스트 카드 내용3'],
-		},
-	];
+	// const yearDiscount = 10;
+	// const cardCount = 2;
+	// const headDiscount = [
+	// 	{ headCount: 5, discountRate: 10 },
+	// 	{ headCount: 10, discountRate: 20 },
+	// 	{ headCount: 15, discountRate: 30 },
+	// 	{ headCount: 20, discountRate: 40 },
+	// ];
+	// const testCard: PriceCard[] = [
+	// 	{
+	// 		title: '테스트 카드',
+	// 		price: 10000,
+	// 		discountRate: 10,
+	// 		detail: '테스트 카드 상세',
+	// 		feature: '테스트 카드 특징',
+	// 		content: ['테스트 카드 내용1', '테스트 카드 내용2', '테스트 카드 내용3'],
+	// 	},
+	// 	{
+	// 		title: '테스트 카드',
+	// 		price: 10000,
+	// 		discountRate: 10,
+	// 		detail: '테스트 카드 상세',
+	// 		feature: '테스트 카드 특징',
+	// 		content: ['테스트 카드 내용1', '테스트 카드 내용2', '테스트 카드 내용3'],
+	// 	},
+	// ];
 
 	const faqTest: FAQCard[] = [
 		{
@@ -84,31 +84,31 @@ export default function EditTemplate() {
 		},
 	];
 
-	useEffect(() => {
-		// testCard 길이만큼 price와 discountRate값을 initialState의 data로 교체한다
-		testCard.forEach((card, index) => {
-			dispatch(
-				updateTierDiscount({
-					index,
-					tierPrice: card.price,
-					discountRate: card.discountRate,
-				}),
-			);
-		});
-		dispatch(setYearDiscount(yearDiscount));
-		headDiscount.forEach((card, index) => {
-			dispatch(
-				updateHeadDiscount({
-					index,
-					headCount: card.headCount,
-					discountRate: card.discountRate,
-				}),
-			);
-		});
-		dispatch(updateCardCount(cardCount));
+	// useEffect(() => {
+	// 	// testCard 길이만큼 price와 discountRate값을 initialState의 data로 교체한다
+	// 	testCard.forEach((card, index) => {
+	// 		dispatch(
+	// 			updateTierDiscount({
+	// 				index,
+	// 				tierPrice: card.price,
+	// 				discountRate: card.discountRate,
+	// 			}),
+	// 		);
+	// 	});
+	// 	dispatch(setYearDiscount(yearDiscount));
+	// 	headDiscount.forEach((card, index) => {
+	// 		dispatch(
+	// 			updateHeadDiscount({
+	// 				index,
+	// 				headCount: card.headCount,
+	// 				discountRate: card.discountRate,
+	// 			}),
+	// 		);
+	// 	});
+	// 	dispatch(updateCardCount(cardCount));
 
-		dispatch(setPriceCard(testCard));
-	}, []);
+	// 	dispatch(setPriceCard(testCard));
+	// }, []);
 
 	const { configState } = useConfig();
 	const { isPreview } = configState;
@@ -123,7 +123,6 @@ export default function EditTemplate() {
 				content: height.toString(),
 			}),
 		);
-
 	};
 
 	const {} = useModal();
@@ -164,6 +163,10 @@ export default function EditTemplate() {
 					<FAQ />
 				</section>
 			</main>
+			<button type="button" onClick={toggleModal}>
+				모달 나와라 얍!
+			</button>
+			{isModalOpen && <PriceModal toggleModal={toggleModal} />}
 		</>
 	);
 }

e9d034283f1eb95fb896162862712598bea08e24
sung34 <imspins1234@gmail.com>
2023-06-18 16:50:42 +0900

    :factory: refactor: 삭제 버튼에서 받는 className을 generic하게 변경
    - group이나 group
    {그룹명} 을 통해 block으로 변경 시 보이는 것으로 변경
    - ex) group-hover
    table:block

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -5,13 +5,16 @@ type Props = {
 	className?: string
 	onClick: () => void | ((id: string) => void)
 };
-
+/**
+ * @description 상위 컴포넌트에서 className='group/{ 임의의 group name}' 을 선언하시고
+ * className='group-hover/{ 임의의 group name }:{ option }' 을 props에 넣어야 보입니다.
+ */
 export default function DeleteButton({className, onClick}: Props) {
 	return (
 		<button
 			type="button"
 			onClick={onClick}
-			className={`absolute hidden -right-3 -top-3 group-hover:block ${className ? className : ""}`}
+			className={`absolute hidden -right-3 -top-3 ${className ? className : ""}`}
 		>
 			<Image src={'/icons/hover_delete.svg'} width={24} height={24} alt='delete button icon svg' />
 		</button>

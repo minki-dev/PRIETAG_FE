619b6c972e79e10e5baaef4651c893c800d23838
jyc-coder <jyc4648@gmail.com>
2023-06-22 19:49:43 +0900

    :hammer: fix: 데이터 수정(modalSlice)
    데이터 직렬화 관련 이슈로 인해 데이터 타입을 수정
    
    ModalProps.cancelButton.onCancel , behaveButton.onAction, 필요없는 주석 제거

--- store/slice/modalSlice.tsx
+++ store/slice/modalSlice.tsx
@@ -2,40 +2,26 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '..';
-
 export interface ModalProps {
 	cancelButton: {
 		text: string;
-		onCancel: () => void;
+		onCancel: (() => void) | null;
 	};
 	behaveButton: {
 		text: string;
 		color: string;
-		onAction: () => void;
+		onAction: (() => void) | null;
 	};
 }
-
 export interface ModalParams {
 	title: string;
 	description: React.ReactNode;
-	buttons: {
-		cancelButton?: {
-			text: string;
-			onCancel: () => void;
-		};
-		behaveButton?: {
-			text: string;
-			color: string;
-			onAction: () => void;
-		};
-	};
+	buttons: ModalProps;
 }
-
 export interface ModalState {
 	isOpen: boolean;
 	params: ModalParams;
 }
-
 const initialState: ModalState = {
 	isOpen: false,
 	params: {
@@ -44,17 +30,16 @@ const initialState: ModalState = {
 		buttons: {
 			cancelButton: {
 				text: '',
-				onCancel: () => {},
+				onCancel: null,
 			},
 			behaveButton: {
 				text: '',
 				color: '',
-				onAction: () => {},
+				onAction: null,
 			},
 		},
 	},
 };
-
 const modalSlice = createSlice({
 	name: 'modal',
 	initialState,
@@ -68,41 +53,14 @@ const modalSlice = createSlice({
 		},
 	},
 });
-
 export const { openModal, closeModal } = modalSlice.actions;
-
 export function useModal() {
 	const dispatch = useDispatch();
 	const { isOpen, params } = useSelector((state: RootState) => state.modal);
-
 	return {
 		dispatch,
 		isOpen,
 		params,
 	};
 }
-
 export default modalSlice.reducer;
-
-{
-	/* 다른 페이지에서 모달 사용하실 때 다음과 같이 작성하시면 됩니다!
-	 
-		'use client';
-
-		import React from 'react';
-		import { openModal, useModal } from '@/store/slice/modalSlice';
-		import { GlobalModal } from '@/components/modal/GlobalModal';
-		import { ModalTypes } from '@/components/modal/ModalState';
-		export default function Home() {
-			const { dispatch } = useModal();
-			return (
-				<div>
-					<button onClick={() => dispatch(openModal(ModalTypes.FAQResetModal))}>
-						버튼
-					</button>
-					<GlobalModal />
-				</div>
-			);
-		}
-		*/
-}

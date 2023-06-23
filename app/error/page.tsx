c58f4507fa8ee9de837c1af46da46dcb2280b503
Franz Cho <franzzcho@gmail.com>
2023-06-23 16:07:00 +0900

    💄design: 디자인 수정

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -13,32 +13,26 @@ export default function error() {
     <>
       <Header />
       <div className='relative flex w-[1920px] h-[808px] justify-between'>
-        <div className='flex'>
-          <div className='w-[240px] h-[808px] bg-[#F9E8E8]' />
-          <div className=' w-[80px] h-[808px] left-[240px] bg-[#FEEEEE]' />
-          <div className='w-[120px] h-[808px] left-[320px] bg-[#F6F6F6]' />
+        <div className='w-[240px] h-[808px] bg-[#F9E8E8]' />
+        <div className='w-[100%] justify-center items-center bg-[#F7F8FC]'>
+          <div className='w-[640px] h-[373px] mt-[180px] ml-[400px]'>
+            <Image
+              src='/img/error_404.svg'
+              alt='Loading...'
+              width={144}
+              height={144}
+              className='mb-[56px] mx-auto'
+            />
+            <h1 className='w-[640px] not-italic font-bold text-[32px] leading-[51px] text-center'>
+              페이지에 연결할 수 없어요.
+            </h1>
+            <h2 className='w-[640px] not-italic font-medium text-2xl leading-[38px] text-[#989898] text-center'>
+              올바른 주소 또는 경로를 확인해 주세요.
+            </h2>
+            <button type='button' onClick={() => router.back()} className='w-[104px] h-[34px] ml-[268px] mt-[48px] bottom-0 border rounded border-solid border-[#747474]'>돌아가기</button>
+          </div>
         </div>
-        <div className='flex flex-col min-w-[375px] justify-center items-center'>
-          <Image
-            src='/img/error_404.svg'
-            alt='Loading...'
-            width={144}
-            height={144}
-            className='absolute top-[186px] mb-[56px]'
-          />
-          <h1 className='w-[640px] not-italic font-bold text-[32px] leading-[51px] text-center mt-[61px] '>
-            페이지에 연결할 수 없어요.
-          </h1>
-          <h2 className='w-[640px] mb-[48px] mt-[8px] not-italic font-medium text-2xl leading-[38px] text-[#989898] text-center'>
-            올바른 주소 또는 경로를 확인해 주세요.
-          </h2>
-          <button type='button' onClick={() => router.back()} className='w-[104px] h-[34px] border rounded border-solid border-[#747474]'>돌아가기</button>
-        </div>
-        <div className='flex'>
-          <div className=' w-[120px] h-[808px] right-[320px] bg-[#F6F6F6]' />
-          <div className=' w-[80px] h-[808px] right-[240px] bg-[#FEEEEE]' />
-          <div className=' w-[240px] h-[808px] right-0 bg-[#F9E8E8]' />
-        </div>
+        <div className=' w-[240px] h-[808px] right-0 bg-[#F9E8E8]' />
       </div>
       <Footer />
     </>

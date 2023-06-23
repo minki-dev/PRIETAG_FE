8477793e238de49cd8880fd8f4826fee52a136bd
Franz Cho <franzzcho@gmail.com>
2023-06-23 16:07:40 +0900

    💄design: 디자인 수정

--- app/Loading/page.tsx
+++ app/Loading/page.tsx
@@ -3,36 +3,31 @@ import Image from 'next/image'
 import Header from '@/components/header/Header'
 import Footer from '@/components/footer/Footer'
 
-export default function Loading() {
+export default function error() {
+
   return (
     <>
       <Header />
-      <div className='relative flex w-[1920px] h-[808px] justify-between'>
-        <div className='flex'>
-          <div className='w-[240px] h-[808px] bg-[#F9E8E8]' />
-          <div className=' w-[80px] h-[808px] left-[240px] bg-[#FEEEEE]' />
-          <div className='w-[120px] h-[808px] left-[320px] bg-[#F6F6F6]' />
+      <div className='relative flex w-[1920px] h-[808px]  justify-between'>
+        <div className='w-[240px] h-[808px] bg-[#F9E8E8]' />
+        <div className='w-[100%] justify-center items-center bg-[#F7F8FC]'>
+          <div className='w-[640px] h-[373px] mt-[180px] ml-[400px] mr-[400px]'>
+            <Image
+              src='/img/loading.svg'
+              alt='Loading...'
+              width={144}
+              height={144}
+              className='mb-[56px] mx-auto'
+            />
+            <h1 className='w-[640px] not-italic font-bold text-[32px] leading-[51px] text-center'>
+              페이지를 불러오고 있어요.
+            </h1>
+            <h2 className='w-[640px] not-italic font-medium text-2xl leading-[38px] text-[#989898] text-center'>
+              로딩 중이니 잠시 기다려 주세요.
+            </h2>
+          </div>
         </div>
-        <div className='flex flex-col min-w-[375px] justify-center items-center'>
-          <Image
-            src='/img/loading.svg'
-            alt='Loading...'
-            width={144}
-            height={144}
-            className='absolute top-[186px] mb-[56px]'
-          />
-          <h1 className='w-[640px] h-[52px] not-italic font-bold text-[32px] leading-[51px] text-center  '>
-            페이지를 불러오고 있어요.
-          </h1>
-          <h2 className='w-[640px] h-[52px] not-italic font-medium text-2xl leading-[38px] text-[#989898] text-center'>
-            로딩 중이니 잠시 기다려 주세요.
-          </h2>
-        </div>
-        <div className='flex'>
-          <div className=' w-[120px] h-[808px] right-[320px] bg-[#F6F6F6]' />
-          <div className=' w-[80px] h-[808px] right-[240px] bg-[#FEEEEE]' />
-          <div className=' w-[240px] h-[808px] right-0 bg-[#F9E8E8]' />
-        </div>
+        <div className=' w-[240px] h-[808px] right-0 bg-[#F9E8E8]' />
       </div>
       <Footer />
     </>

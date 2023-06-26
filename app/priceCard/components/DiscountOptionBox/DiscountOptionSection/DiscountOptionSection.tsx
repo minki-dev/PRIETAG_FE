2ea71cfb728062e20375239366e7890e3e4002b0
김민기 <kimminki.dev@gmail.com>
2023-06-27 08:06:12 +0900

    :sparkles: feat: - 색상 설정에 따라 할인 설정 영역에 변경된 색상 반영
    - 월간, 연간 토글 버튼에서 연간 체크 시 연간 부분 글자 색상에 설정 반영
    - 사용자 수 입력부분 숫자 색상에 설정 반영

--- app/priceCard/components/DiscountOptionBox/DiscountOptionSection/DiscountOptionSection.tsx
+++ app/priceCard/components/DiscountOptionBox/DiscountOptionSection/DiscountOptionSection.tsx
@@ -3,28 +3,20 @@ import React from 'react';
 import MonthYearChecked from './MonthYearChecked';
 import UserCountChecked from './UserCountChecked';
 import { usePriceModal } from '@/store/slice/priceModalSlice';
-
-interface colorInfo {
-	mainColor: string;
-	subColor01: string;
-	subColor02: string;
-}
+import { useConfig } from '@/store/slice/configSlice';
 
 function DiscountOptionSection() {
-	const colorInfoEl: colorInfo = {
-		mainColor: '#00A3FF',
-		subColor01: '#60C8FF',
-		subColor02: '#EAF8FF',
-	};
 	const { priceModal, dispatch } = usePriceModal();
+	const { configState } = useConfig();
+	const { color } = configState;
 
 	return (
 		<div className="editable-inner flex justify-center gap-10">
 			{priceModal.isCheckPerYear ? (
-				<MonthYearChecked color={colorInfoEl} />
+				<MonthYearChecked color={color.mainColor} />
 			) : null}
 			{priceModal.pricing === '정량제' ? (
-				<UserCountChecked color={colorInfoEl} />
+				<UserCountChecked color={color.mainColor} />
 			) : null}
 		</div>
 	);

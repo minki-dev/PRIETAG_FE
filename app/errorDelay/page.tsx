fcbc33b4bea0660d2616f5dab9cd8cb1d990da38
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-06-27 19:43:28 +0900

    💄 design: 반응형시 나타나는 화면 여백 에러 수정

--- app/errorDelay/page.tsx
+++ app/errorDelay/page.tsx
@@ -4,27 +4,29 @@ import Footer from '@/components/footer/Footer';
 
 function ErrorDelay() {
 	return (
-		<div className=" flex h-full w-[375px] flex-col items-center justify-center bg-transparent px-[20px] sm:w-[1280px] sm:px-[120px] xl:w-[1920px]  xl:bg-[#F9E8E9] xl:px-[240px]">
+		<div>
 			<Header />
-			<div className=" flex h-full w-full flex-col items-center justify-center bg-[#F7F8FC] pt-[72px] text-center xl:h-[808px]">
-				<div className=" h-[120px] w-[120px] sm:h-[144px] sm:w-[144px]">
-					<img
-						src="/img/error_slow.svg"
-						className=" object-cover "
-						alt="연결느림 이미지"
-					/>
-				</div>
-				<div className="my-[48px] leading-[51px]">
-					<div className=" text-[20px] font-bold sm:text-[32px]">
-						잠시 연결이 늦어지고 있습니다.{' '}
+			<div className=" flex h-full w-[375px] flex-col items-center justify-center bg-transparent px-[20px] sm:w-full sm:min-w-[1280px] sm:px-[120px] xl:w-full xl:min-w-[1480px] xl:bg-[#F9E8E9] xl:px-[240px]">
+				<div className=" flex h-full w-full flex-col items-center justify-center bg-[#F7F8FC] pt-[72px] text-center xl:h-[808px]  ">
+					<div className=" h-[120px] w-[120px] sm:h-[144px] sm:w-[144px]">
+						<img
+							src="/img/error_slow.svg"
+							className=" object-cover "
+							alt="연결느림 이미지"
+						/>
 					</div>
-					<div className="text-base font-medium text-[#989898]  sm:text-2xl">
-						조금 뒤 다시 시도해 주세요.
+					<div className="my-[48px] leading-[51px]">
+						<div className=" text-[20px] font-bold sm:text-[32px]">
+							잠시 연결이 늦어지고 있습니다.{' '}
+						</div>
+						<div className="text-base font-medium text-[#989898]  sm:text-2xl">
+							조금 뒤 다시 시도해 주세요.
+						</div>
 					</div>
+					<button className="h-[34px] w-[104px] rounded border-[1px] border-[#747474] px-[18px] py-[5.5px] text-sm font-medium">
+						다시 시도
+					</button>
 				</div>
-				<button className="h-[34px] w-[104px] rounded border-[1px] border-[#747474] px-[18px] py-[5.5px] text-sm font-medium">
-					다시 시도
-				</button>
 			</div>
 			<Footer />
 		</div>

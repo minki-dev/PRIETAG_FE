3015aa6f4caaacc45ef0964b85753545260b3361
jyc-coder <jyc4648@gmail.com>
2023-06-28 16:20:43 +0900

    :sparkles: feat: layout에 AuthSession 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import React from 'react';
 import ReduxProvider from '@/store/ReduxProvider';
+import AuthSession from './api/auth/[...nextauth]/AuthSession';
 const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({
 	children,
@@ -15,7 +16,7 @@ export default function RootLayout({
 				<ReduxProvider>
 					<main className="min-h-screen w-screen bg-gray-100">
 						<main className=" m-auto w-[375px]  bg-white sm:w-full xl:min-w-[1280px] xl:max-w-[1920px]  ">
-							{children}
+							<AuthSession>{children}</AuthSession>
 						</main>
 					</main>
 				</ReduxProvider>

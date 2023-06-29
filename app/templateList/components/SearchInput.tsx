3d34f3c805cc46dc70d2ca5e2829a79bd5be6930
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-06-29 14:19:11 +0900

    🏭refactor: 이벤트 객체 타입 지정

--- app/templateList/components/SearchInput.tsx
+++ app/templateList/components/SearchInput.tsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import Image from 'next/image';
 import { useDispatch } from 'react-redux';
 import versionSlice, { filterByKeyword } from '@/store/slice/versionSlice';
 
 function SearchInput() {
 	const dispatch = useDispatch();
-	const [keyword, setKeyword] = useState('');
-	const handleSearch = (e) => {
+	const [keyword, setKeyword] = useState<string>('');
+	const handleSearch = (e: SyntheticEvent) => {
 		e.preventDefault();
 		dispatch(filterByKeyword(keyword));
 	};
 	return (
 		<div className="relative flex h-[46px] w-[400px] items-center justify-between">
-			<form className="h-full" onSubmit={(e) => e.preventDefault()}>
+			<form
+				className="h-full"
+				onSubmit={(e: SyntheticEvent) => e.preventDefault()}
+			>
 				{' '}
 				<input
 					type="text"
 					placeholder="검색어를 입력해 주세요"
 					className="shadow-[0_0px_4px_rgba(0, 0, 0, 0.35)] absolute right-0 mt-[10px] min-w-[400px] rounded-md px-[16px] py-[10px] shadow"
 					value={keyword}
-					onChange={(e) => {
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
 						setKeyword(e.target.value);
 					}}
 				/>

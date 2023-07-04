c9214ec9a6de41f9bf80b3911c345153cd7c259e
jyc-coder <jyc4648@gmail.com>
2023-07-04 13:46:30 +0900

    :sparkles: feat: nextAuth redirect_uri 변경

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,7 +9,7 @@ const handler = NextAuth({
 			clientSecret: `${process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET}` || '',
 			authorization: {
 				params: {
-					redirect_uri: `https://ezfee.vercel.app`,
+					redirect_uri: `http://localhost:3000`,
 				},
 			},
 		}),
@@ -25,7 +25,7 @@ const handler = NextAuth({
 			return baseUrl;
 		},
 	},
-	secret: process.env.NEXTAUTH_SECRET
+	secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
